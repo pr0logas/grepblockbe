e90c31286ab263361b5e26f84981f3399931fa55
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-11-03 14:40:43 +0200

    no divider

--- parseWallets.sh
+++ parseWallets.sh
@@ -104,7 +104,7 @@ for (( ; ; ))
                                      stopCountingProcessTime2
                                      runtime2=$((end2-start2))
                                      setDateStamp=$(date +%Y-%m-%d\|%H:%M:%S\|%N)
-                                     echo -e "${BLUE} $setDateStamp Txid aggregation done: $i $runtime2 ms ${NC}"
+                                     echo -e "${BLUE}$setDateStamp Txid aggregation done: $i $runtime2 ms ${NC}"
 
 
                 elif [ $? -eq 5 ]; then
@@ -120,5 +120,5 @@ for (( ; ; ))
                                     stopCountingProcessTime1
                                     runtime1=$((end1-start1))
                                     setDateStamp=$(date +%Y-%m-%d\|%H:%M:%S\|%N)
-                                    echo -e "${GREEN} $setDateStamp Block aggregation finished: $checkLastProgressIncreased Overall processing took: $runtime1 ms ${NC}"
+                                    echo -e "${GREEN}$setDateStamp Block aggregation finished: $checkLastProgressIncreased Overall processing took: $runtime1 ms ${NC}"
 done
