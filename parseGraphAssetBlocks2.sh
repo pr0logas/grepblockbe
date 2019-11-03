26d27496b1ed1bcbd07e9a3eea8c97932799d8e8
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-11-03 14:44:04 +0200

    Zcash testing

--- parseGraphAssetBlocks2.sh
+++ parseGraphAssetBlocks2.sh
@@ -74,7 +74,7 @@ if [ $? -eq 0 ]; then
                         formatNextDayRevA=$(echo $NextDayRevA | sed 's@0@@g')
 			
                         # Check if it's next day or not?
-                        if [[ $formatThisDayRevA -eq $formatNextDayRevA ]] && [[ $sumBlocks > 10 ]] ; then
+                        if [[ $formatThisDayRevA -eq $formatNextDayRevA ]] && [[ $sumBlocks -gt 10 ]] ; then
                                 # Format JSON
                                 sed -i '$ d' $formatingFile
                                 sed -i '$ d' $formatingFile
