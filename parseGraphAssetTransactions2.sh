1042a8a45eaceaf150bc352a310cf1c0777a2daf
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-11-03 13:48:38 +0200

    Increase tx size

--- parseGraphAssetTransactions2.sh
+++ parseGraphAssetTransactions2.sh
@@ -75,7 +75,7 @@ if [ $? -eq 0 ]; then
                         formatNextDayRevA="$(echo $NextDayRevA | sed 's@0@@g')"
 
                         # Check if it's next day or not?
-                        if [[ $formatThisDayRevA -eq $formatNextDayRevA ]] && [[ $sumTxs > 50 ]] ; then
+                        if [[ $formatThisDayRevA -eq $formatNextDayRevA ]] && [[ $sumTxs > 90 ]] ; then
 
 
                                 # Format JSON
